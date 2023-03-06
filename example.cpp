44b4dffc7812c8b2a19d7493f16f44e8b8579ad0
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-03-06 17:06:50 +0700

    first commit

--- example.cpp
+++ example.cpp
@@ -1,22 +1,23 @@
-//library
-#include <iostream> 
+//Library
+#include <iostream>
 using namespace std;
 
 //program main
-int main(){ //begin
+int main(){//begin
 
     //numeric p,l,luas
-    double p,l,luas
-
-    //display masukkan panjang
-    cout<<"masukkan panjang =";
+    double p,l,luas;
+    //display "masukan panjang"
+    cout<<"masukan panjang = ";
     //accept p
-    cin<<"p";
-    cout<<"masukkan lebar =";
-    cin<<"l";
-    //compute luas = p*l
-    luas = p*l;
-    //display "luas persegi panjang adalah", luas
-    cout<<"luas persegi panjang adalah " <<luas;
+    cin>>p;
+    //display "masukan lebar"
+    cout<<"masukan lebar = ";
+    //accept l
+    cin>>l;
+    //compute luas=p*l
+    luas=p*l;
+    //display "Luas persegi panjang adalah", Luas
+    cout<<"luas persegi panjang adalah "<<luas;
 
 }//end
